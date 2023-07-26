bac3e9386a9bec3ff5fb1321765015e23fa95aba
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-26 13:56:19 +0200

    - Set up the project - Create folder to get a structure - Create the NetwokingManager

---
+++ TheMovieDB-V2/Base/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TheMovieDB-V2
+//
+//  Created by Eric on 26/07/2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiKey = "11c5611fd9b625d18ecf5e2ea326b6cb"
+    static let imgUrl = "https://image.tmdb.org/t/p/w500/"
+    static let imgOriginalUrl = "https://image.tmdb.org/t/p/original/"
+}
